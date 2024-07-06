13c6bcff04573b2ce915f4a8d80bc4fc897b3abc
dor <dori199791@gmail.com>
2024-07-06 19:20:10 +0300

    c2

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
+from pymongo import MongoClient
+import firebase_admin
+from firebase_admin import credentials, auth
+
+#Firebase
+cred = credentials.Certificate("C:\Users\dori1\project\BS-PMC-2024-Team4\backend\dogworry-37217-firebase-adminsdk-sdrex-9460d7f8ff.json")
+firebase_admin.initialize_app(cred)
+
+#MongoDb
+uri = "mongodb+srv://doryu:JXG6z9PaWmQYFVqD@dogworry.iutobjz.mongodb.net/?retryWrites=true&w=majority&appName=DogWorry"
+client = MongoClient(uri)
+try:
+    database = client.get_database("sample_mflix")
+    movies = database.get_collection("movies")
+    # Query for a movie that has the title 'Back to the Future'
+    query = { "title": "Back to the Future" }
+    movie = movies.find_one(query)
+    print(movie)
+    client.close()
+except Exception as e:
+    raise Exception("Unable to find the document due to the following error: ", e)
 
 
 app = Flask(__name__)
