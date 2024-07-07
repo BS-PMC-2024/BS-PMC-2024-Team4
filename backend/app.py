9f9ef41352adead78f96997608bf68801cf1d3c7
danieBa6 <bar.daniel.12@gmail.com>
2024-07-07 15:43:10 +0300

    with index.html for firebase

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
 
+#for mongo-db
+import pymongo
+
+#for firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+
+#cred = credentials.Certificate('dogworry-37217-firebase-adminsdk-sdrex-9460d7f8ff.json')
+# Initialize the app with a service account, granting admin privileges
+#firebase_admin.initialize_app(cred, {'databaseURL': "URL to database"})
+
+### end of firebase
+
+
+
 app = Flask(__name__)
 CORS(app)
 
