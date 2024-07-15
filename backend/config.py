b457e3fdf125d0de9b09ced4261c1ac9d1677092
danieBa6 <bar.daniel.12@gmail.com>
2024-07-15 16:22:25 +0300

    backend changes not important

--- backend/config.py
+++ backend/config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     MONGO_URI = "mongodb+srv://yovelni:Ecz4680zZqj8OOEv@dogworry.iutobjz.mongodb.net/?retryWrites=true&w=majority&appName=DogWorry"
+    
