2530229e33edf6f709f1ae7fe694e7a5e6409b19
dor <dori199791@gmail.com>
2024-07-18 10:52:03 +0300

    fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -37,17 +37,7 @@ def create_app(config_class=Config):
     from app.lostdogs import ld as ld_bp
     app.register_blueprint(ld_bp, url_prefix='/lostDog')
 
-    from app.manager import bp as manager_bp
-    app.register_blueprint(manager_bp, url_prefix='/manager')
 
-    from app.lostdogs import ld as ld_bp
-    app.register_blueprint(ld_bp, url_prefix='/lostDog')
-
-    from app.manager import bp as manager_bp
-    app.register_blueprint(manager_bp, url_prefix='/manager')
-
-    from app.lostdogs import ld as ld_bp
-    app.register_blueprint(ld_bp, url_prefix='/lostDog')
 
     @app.route('/test/')
     def test_page():
