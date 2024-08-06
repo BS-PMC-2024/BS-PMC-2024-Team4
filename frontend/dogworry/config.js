e527e46d2304268c9002486184c6e0893bc364f8
danieBa6 <bar.daniel.12@gmail.com>
2024-08-06 22:28:59 +0300

    .

--- frontend/dogworry/config.js
+++ frontend/dogworry/config.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-const api_url =  'http://192.168.1.244:5000/';
-=======
-const api_url =  'http://10.0.0.13:5000/';
->>>>>>> main
+const api_url =  'http://192.168.31.175:5000/';
 
 export default api_url;
